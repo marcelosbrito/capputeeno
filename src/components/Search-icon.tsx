7e89444cd8363fd0a4cdcef7662b9e978164d2d0
Marcelo Brito <marcellus.brito@gmail.com>
2023-11-09 00:26:30 -0300

    Component Header created

---
+++ src/components/Search-icon.tsx
@@ -0,0 +1,9 @@
+export function SearchIcon(){
+  return(
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+    <circle cx="11.059" cy="11.059" r="7.062" stroke="#737380" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"/>
+    <path stroke="#737380" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m20.003 20.003-3.951-3.951"/>
+    </svg>
+
+  )
+}
