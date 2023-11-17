8a1c26e256e232ba1cd355ba402a73cbb8b09659
Marcelo Brito <marcellus.brito@gmail.com>
2023-11-17 00:23:36 -0300

    feat: initial products fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+"use client"
+
 import Image from 'next/image'
 import styles from './page.module.css'
 import { FilterBar } from '@/components/Filter-bar'
+import { ProductsList } from '@/components/Products-list'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function Home() {
+  const client = new QueryClient();
   return (
-    <main className={styles.main}>
-      <FilterBar/>
-    </main>
+    <QueryClientProvider client={client}>
+      <main className={styles.main}>
+        <FilterBar/>
+        <ProductsList />
+      </main>
+    </QueryClientProvider>
   )
 }
