af7bee9f3751dd113593444bd1a3b94e3e60666b
Marcelo Brito <marcellus.brito@gmail.com>
2023-11-24 22:56:35 -0300

    Fix: fix filters responsivity

--- src/components/Primary-input.tsx
+++ src/components/Primary-input.tsx
@@ -3,27 +3,36 @@ import { styled } from "styled-components";
 import { SearchIcon } from "./icons/Search-icon";
 
 export const PrimaryInput = styled.input`
-  width: 352px;
+  width: 100%;
   border-radius: 8px;
   padding: 10px 16px;
   background: var(--bg-secondary);
   border: none;
   font-family: inherit;
-  font-size: 14px;
+  font-size: 12px;
   font-weight: 400;
-  line-height: 22px;
+  line-height: 20px;
   color: var(--text-dark);
+
+  @media (min-width: ${props => props.theme.desktopBreakpoint}){
+    font-size: 14px;
+    line-height: 22px;
+  }
 `
 
 const InputContainer = styled.div`
   position: relative;
-  width: 352px;
+  width: 250px;
 
   svg {
     position:absolute;
     right: 20px;
     top: 50%;
     transform: translateY(-50%);
+  }
+
+  @media (min-width: ${props => props.theme.desktopBreakpoint}){
+    width: 352px;
   }
 `
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
